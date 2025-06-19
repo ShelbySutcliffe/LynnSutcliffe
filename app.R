5497ee23f17015e82088579cd4205a51c3f02229
ShelbySutcliffe <shelbysodonnell@gmail.com>
2025-06-18 20:37:47 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,88 +1,74 @@
 library(shiny)
-library(shinythemes)
+
+# Define album data
+albums <- list(
+  "SLS" = "SLS.jpg",
+  "SLS_BellevueBuddies" = "SLS_BellevueBuddies.jpg",
+  "SLS_Bernardsville" = "SLS_Bernardsville.jpg",
+  "SLS_HisCareer" = "SLS_HisCareer.jpg",
+  "SLS_JuniorHighSweethearts" = "SLS_JuniorHighSweethearts.jpg",
+  "SLS_OurDad" = "SLS_OurDad.jpg",
+  "SLS_OurGrandad" = "SLS_OurGrandad.jpg",
+  "SLS_OverTheYears" = "SLS_OverTheYears.jpg",
+  "SLS_PrincetonPeeps" = "SLS_PrincetonPeeps.jpg"
+)
 
 ui <- fluidPage(
-  theme = shinytheme("flatly"),
-  tags$head(tags$style(HTML(".album-img { width: 100%; max-width: 250px; border-radius: 15px; margin-bottom: 10px; } .album-block { display: inline-block; margin: 15px; text-align: center; vertical-align: top; } .main-cover { width: 100%; max-width: 500px; border-radius: 20px; margin-bottom: 20px; } .special-button { background-color: #2e6b3d; color: white; padding: 10px 20px; border-radius: 10px; font-size: 18px; text-decoration: none; display: inline-block; margin: 20px 0; } h2, h3, a { font-family: 'Lucida Handwriting', cursive; color: #2c3e50; }"))),
-
-  titlePanel(""),
-
- tags$div(style = "text-align:center",
-         tags$h2("S. Lynn Sutcliffe 1943–2024"),
-         tags$h3("A Celebration of His Life")
-),
-
-  tags$div(style = "text-align:center",
-    tags$a(href = "https://drive.google.com/drive/folders/1Puep4x6BewUvSiwDsHibGBkhHewcpRYD?usp=sharing",
-           class = "special-button", target = "_blank",
-           "Virtual or In-Person Celebration of Life Guests: Add Your Photos Here")
+  tags$head(
+    tags$style(HTML("
+      .instruction-text {
+        font-size: 20px;
+        font-weight: bold;
+        margin-bottom: 20px;
+        text-align: center;
+      }
+      .album-img {
+        width: 100%;
+        height: auto;
+        border-radius: 8px;
+        box-shadow: 0px 0px 5px #888;
+      }
+      .album-title {
+        font-size: 20px;
+        font-weight: bold;
+        margin-top: 8px;
+        text-align: center;
+      }
+      .album-button {
+        font-size: 18px;
+        padding: 10px 20px;
+        margin-top: 10px;
+        width: 100%;
+      }
+      @media (max-width: 768px) {
+        .album-button {
+          font-size: 20px;
+        }
+      }
+    "))
   ),
-
-  tags$div(style = "text-align:center",
-    tags$a(href = "https://docs.google.com/forms/d/e/1FAIpQLSehYnXp0lpBA4JuVN54xPkuabPiM-qE6s3m98qfOfEcjzDUYw/viewform?usp=header",
-           class = "special-button", target = "_blank",
-           "Sign the Guest Book")
-  ),
-
-  tags$div(style = "text-align:center",
-    tags$h3("Click an album below to view photos")
-  ),
-
+  
+  div(class = "instruction-text", "Click on album TITLE below to view photos"),
+  
   fluidRow(
-    column(12, align = "center",
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/sls.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/10I-5gDPnreyFNKtDJA9L16cp4ON7suUi?usp=sharing", target = "_blank", "SLS")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/bellevue.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/14s23K6wEK_Av_Dk-fqkX71dmh7RiYDcN?usp=sharing", target = "_blank", "Bellevue Buddies")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/bernardsville.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/0ByG4Ynkjp669aHZsWVFoMXJaUTg?resourcekey=0-HFdxtnOrW_cYI1EbAjWv-Q&usp=sharing", target = "_blank", "Bernardsville")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/career.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/1yyBEOMKQ92IfBRv4l1e_5yXWkucfOGQv?usp=sharing", target = "_blank", "His Career")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/ourdad.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/1qoXjkCM_cF0GeLoopJJJMXQ6ef3mFkwf?usp=sharing", target = "_blank", "Our Dad")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/ourgrandad.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/1Lk0Z0hqWnTB1BXIhrQOdGzw69arsWec3?usp=sharing", target = "_blank", "Our Grandad")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/years.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/14oE8fhDCk4eVAd5JIz6xnQkJKNR26A1Y?usp=sharing", target = "_blank", "Over the Years")
-      ),
-      tags$div(class = "album-block",
-        tags$img(src = "https://raw.githubusercontent.com/ShelbySutcliffe/LynnSutcliffe/main/princeton.jpg", class = "album-img"),
-        tags$a(href = "https://drive.google.com/drive/folders/1EEeXlSzc_NLNusY_PVd9SIOwxhSPM12_?usp=sharing", target = "_blank", "Princeton Peeps")
+    lapply(names(albums), function(album) {
+      column(
+        width = 4,
+        div(
+          img(src = albums[[album]], class = "album-img"),
+          div(class = "album-title", album),
+          actionButton(inputId = paste0("btn_", album), label = "View Photos", class = "album-button")
+        )
       )
-    )
-  ),
-
-  tags$br(),
-
-  tags$div(style = "text-align:center",
-    tags$h3("Video Tributes"),
-    tags$h4("Watch 'The Boys of Fall '64"),
-    tags$a(href = "https://www.youtube.com/watch?v=axIQPiE-StU", target = "_blank",
-           tags$img(src = "https://img.youtube.com/vi/axIQPiE-StU/0.jpg", width="300px", style="border-radius:10px;")),
-    tags$br(), tags$br(),
-    tags$h4("Watch 'Cath & Lynn: Junior High Sweethearts"),
-    tags$a(href = "https://www.youtube.com/watch?v=zIEk_ebHFaU", target = "_blank",
-           tags$img(src = "https://img.youtube.com/vi/zIEk_ebHFaU/0.jpg", width="300px", style="border-radius:10px;"))
+    })
   )
 )
 
 server <- function(input, output, session) {
-  # No server logic needed for this static app
+  # Server logic placeholder
 }
 
 shinyApp(ui, server)
+
 
 
